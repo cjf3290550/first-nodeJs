9c7a52c753f25a2fb08e37c3af7bef7f5fac4ae1
cjf3290550 <cjf3290550@163.com>
2018-09-03 10:03:24 +0800

    test

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+    "env": {
+        "node": true,
+        "commonjs": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parser": "babel-eslint",
+    "parserOptions": {
+        "ecmaVersion": 2015,
+        "sourceType" : "script"
+    },
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+      "no-console": [
+          "error", {
+          "allow" : ["warn","error","info"]
+           }
+        ]
+    }
+};
---
+++ src/index.js
@@ -0,0 +1,17 @@
+console.log(123);
+console.error(123);
+
+const a = 2;
+
+/*eslint-disable*/
+console.log(a);
+/*eslint-enable*/
+
+console.log(2) //eslint-disable-line
+
+
+/*eslint-disable no-console*/
+console.log(str);
+
+
+var str = 'str';
